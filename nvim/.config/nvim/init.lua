982bab8459ccaf6c2b35216e7f579742c475901b
Leopoldo Rosas <leopoldo.rosas@hlag.com>
2025-04-30 14:36:37 +0200

    New options nvim/tmux
    Zk Note taking
    fix clipboard wsl
    tmux VIM motion

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,6 +14,27 @@ if not pcall(require, "lazy") then
   vim.fn.getchar()
   vim.cmd.quit()
 end
+-- Some OS detectors
+local is_wsl = vim.fn.has("wsl") == 1
+local opt = vim.opt
+local g = vim.g
+-- WSL Clipboard support
+if is_wsl then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
 
 require "lazy_setup"
 require "polish"
